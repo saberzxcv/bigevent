03c4ecc978b3cfe6910b35d6e8c2a02e43a2ddc0
shb <1820897464@qq.com>
2021-01-31 11:10:10 +0800

    修改

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -14,27 +14,33 @@ $(function () {
   // 1.3 创建裁剪区域
   $image.cropper(options)
 
+  //调用file复选框的点击事件。
   $('#btnChooseImage').on('click', function () {
     $('#file').click()
   })
 
-
+  //注册filee复选框改变事件。
   $('#file').on('change', function (e) {
-    // console.log(e);
+    console.log(e);
+    //e.target.files属性,获取用户输入的文件。
     var filelist = e.target.files
+    //判断是否输入文件
     if (filelist.length === 0) {
       return layer.msg('请选择图片！')
     }
 
+    //拿到传入的文件
     var file = e.target.files[0]
+    //cropper插件,转换为url地址
     var imgURL = URL.createObjectURL(file)
+    //重新设置裁剪区域
     $image
       .cropper('destroy') // 销毁旧的裁剪区域
       .attr('src', imgURL) // 重新设置图片路径
       .cropper(options) // 重新初始化裁剪区域
   })
 
-
+  //为确定按钮注册点击事件
   $('#btnUpload').on('click', function () {
     var dataURL = $image
       .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
@@ -46,6 +52,7 @@ $(function () {
     $.ajax({
       method: 'post',
       url: '/my/update/avatar',
+      //avater参数 接受数据
       data: {
         avatar: dataURL
       },
@@ -54,6 +61,7 @@ $(function () {
           return layer.msg('更换头像失败')
         }
         layer.msg('更换头像成功！')
+        //调用getUSerInfo,重新渲染头像。
         window.parent.getUserInfo()
       }
     })
