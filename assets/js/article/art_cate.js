4a9a52d78906ae5b444d203e5e2675f624584902
shb <1820897464@qq.com>
2021-02-01 17:02:57 +0800

    文章管理

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,15 +1,28 @@
-$(function() {
+$(function () {
   var layer = layui.layer
   var form = layui.form
 
   initArtCateList()
 
   // 获取文章分类的列表
+  // function initArtCateList() {
+  //   $.ajax({
+  //     method: 'GET',
+  //     url: '/my/article/cates',
+  //     success: function (res) {
+  //       var htmlStr = template('tpl-table', res)
+  //       $('tbody').html(htmlStr)
+  //     }
+  //   })
+  // }
+
+  //获取文章分类列表的数据,并使用模板引擎填充到表格中
   function initArtCateList() {
     $.ajax({
-      method: 'GET',
+      method: 'get',
       url: '/my/article/cates',
-      success: function(res) {
+      success(res) {
+        console.log(res);
         var htmlStr = template('tpl-table', res)
         $('tbody').html(htmlStr)
       }
@@ -17,30 +30,55 @@ $(function() {
   }
 
   // 为添加类别按钮绑定点击事件
+  //设置索引
+  //layui的内置方法,添加完数据后自动关闭弹出层。
   var indexAdd = null
-  $('#btnAddCate').on('click', function() {
+  $('#btnAddCate').on('click', function () {
     indexAdd = layer.open({
       type: 1,
       area: ['500px', '250px'],
       title: '添加文章分类',
+      // content: '我是猪',content属性。
       content: $('#dialog-add').html()
     })
   })
 
   // 通过代理的形式，为 form-add 表单绑定 submit 事件
-  $('body').on('submit', '#form-add', function(e) {
+  //on:事件委托和为未来的元素添加点击事件。
+
+
+  // $('body').on('submit', '#form-add', function (e) {
+  //   e.preventDefault()
+  //   $.ajax({
+  //     method: 'POST',
+  //     url: '/my/article/addcates',
+  //     data: $(this).serialize(),
+  //     success: function (res) {
+  //       if (res.status !== 0) {
+  //         return layer.msg('新增分类失败！')
+  //       }
+  //       initArtCateList()
+  //       layer.msg('新增分类成功！')
+  //       // 根据索引，关闭对应的弹出层
+  //       //layui的内置方法,添加完数据后自动关闭弹出层。
+  //       layer.close(indexAdd)
+  //     }
+  //   })
+  // })
+
+  $('body').on('submit', '#form-add', function (e) {
     e.preventDefault()
     $.ajax({
-      method: 'POST',
+      method: 'post',
       url: '/my/article/addcates',
       data: $(this).serialize(),
-      success: function(res) {
+      success(res) {
         if (res.status !== 0) {
-          return layer.msg('新增分类失败！')
+          return layer.msg('新增分类失败')
         }
+        //调用模板引擎,重新渲染页面。
         initArtCateList()
-        layer.msg('新增分类成功！')
-        // 根据索引，关闭对应的弹出层
+        layer.msg('新增分类成功')
         layer.close(indexAdd)
       }
     })
@@ -48,7 +86,7 @@ $(function() {
 
   // 通过代理的形式，为 btn-edit 按钮绑定点击事件
   var indexEdit = null
-  $('tbody').on('click', '.btn-edit', function() {
+  $('tbody').on('click', '.btn-edit', function () {
     // 弹出一个修改文章分类信息的层
     indexEdit = layer.open({
       type: 1,
@@ -62,20 +100,20 @@ $(function() {
     $.ajax({
       method: 'GET',
       url: '/my/article/cates/' + id,
-      success: function(res) {
+      success: function (res) {
         form.val('form-edit', res.data)
       }
     })
   })
 
   // 通过代理的形式，为修改分类的表单绑定 submit 事件
-  $('body').on('submit', '#form-edit', function(e) {
+  $('body').on('submit', '#form-edit', function (e) {
     e.preventDefault()
     $.ajax({
       method: 'POST',
       url: '/my/article/updatecate',
       data: $(this).serialize(),
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('更新分类数据失败！')
         }
@@ -87,14 +125,17 @@ $(function() {
   })
 
   // 通过代理的形式，为删除按钮绑定点击事件
-  $('tbody').on('click', '.btn-delete', function() {
+  $('tbody').on('click', '.btn-delete', function () {
     var id = $(this).attr('data-id')
     // 提示用户是否要删除
-    layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
+    layer.confirm('确认删除?', {
+      icon: 3,
+      title: '提示'
+    }, function (index) {
       $.ajax({
         method: 'GET',
         url: '/my/article/deletecate/' + id,
-        success: function(res) {
+        success: function (res) {
           if (res.status !== 0) {
             return layer.msg('删除分类失败！')
           }
